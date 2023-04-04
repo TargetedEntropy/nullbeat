d62ec2ecc9c26f4a022e28c455e5c73a81c6c9a4
Brian Merriam <merriam@gmail.com>
2023-04-04 22:50:40 +0000

    no viewer

--- kitbot.js
+++ kitbot.js
@@ -48,10 +48,10 @@ let options = {
 
 const bot = mineflayer.createBot(options);
 
-const { mineflayer: mineflayerViewer } = require('prismarine-viewer')
-bot.once('spawn', () => {
-  mineflayerViewer(bot, { port: 3007, firstPerson: true }) // port is the web server port, if first person is false, you get a bird's-eye view
-})
+// const { mineflayer: mineflayerViewer } = require('prismarine-viewer')
+// bot.once('spawn', () => {
+//   mineflayerViewer(bot, { port: 3007, firstPerson: true }) // port is the web server port, if first person is false, you get a bird's-eye view
+// })
 
 bindEvents(bot);
 function bindEvents(bot) {
